a076b3ae825fa303d18dcd8cbf87451daa789513
asakurastar <asakurastar@gmail.com>
2011-04-11 17:28:26 -0300

    Plugin atualizado

--- jquery.contador.js
+++ jquery.contador.js
@@ -2,61 +2,58 @@
  * Em desenvolvimento
  */
 (function($) {
-	$.fn.extend({
-		fiveContador: function(settings) {
-			var config = {
-				inicio: 0,
-				fim: 60,
-				cor: '#000088',
-				delay: 1000,
-				onStart: function() {},
-				onUpdate: function() {},
-				onComplete: function() {}
-			};
+    $.fn.extend({
+        fiveContador: function(settings) {
+            var config = {
+                inicio: 0,
+                fim: 60,
+                cor: '#000088',
+                delay: 1000,
+                onStart: function() {},
+                onUpdate: function() {},
+                onComplete: function() {}
+            };
 
-			if (settings) {
-				$.extend(config, settings);
-			}
+            if (settings) {
+                $.extend(config, settings);
+            }
 
-			var me = $(this);
+            var me = $(this);
 
-			return this.each(function() {
-				var methods = {
-					timer: undefined,
+            return this.each(function() {
+                var methods = {
+                    timer: undefined,
 
-					progresso: '<div style="width: 0px; height: ' + $(me).height() + 'px; background-color: ' + config.cor + ';" class="progresso"></div>',
+                    progresso: '<div style="width: 0px; height: ' + $(me).height() + 'px; background-color: ' + config.cor + ';" class="progresso"></div>',
 
-					init: function() {
-						$(me).html(methods.progresso);
+                    init: function() {
+                        $(me).html(methods.progresso);
 
-						if (typeof(methods.timer) == "undefined") {
-							config.onStart();
+                        config.onStart();
+						
+                        methods.updateProgresso();
+                    },
+
+                    updateProgresso: function() {
+                        $(me).find('.progresso').animate({ 
+							'width': '100%' 
+						}, 
+						{
+                            duration: config.delay * config.fim,
 							
-							methods.timer = setInterval(methods.updateProgresso, config.delay);
-						}
-					},
+                            step: function(now, fx) {
+                                config.onUpdate(now.toFixed(2));
+                            },
+							
+                            complete: function() {
+                                config.onComplete();
+                            }
+                        });
+                    }
+                };
 
-					updateProgresso: function() {
-						percentual = parseInt((100 * config.inicio) / config.fim);
-
-						if (config.inicio < config.fim + 1) {
-							$(me).find('.progresso').css('width', percentual + '%');
-							config.inicio++;
-
-							value = percentual;
-							config.onUpdate(value);
-						}
-						else {
-							if (typeof(methods.timer) != "undefined") {
-								clearInterval(methods.timer);
-								config.onComplete();
-							}
-						}
-					}
-				};
-
-				methods.init();
-			});
-		}
-	});
+                methods.init();
+            });
+        }
+    });
 })(jQuery);
