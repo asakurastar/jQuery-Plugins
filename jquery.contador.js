06af46104ccbeb63ff1194576943304c5a5ccb2a
asakurastar <asakurastar@gmail.com>
2011-04-12 14:28:33 -0300

    Adicionado documentação

--- jquery.contador.js
+++ jquery.contador.js
@@ -1,5 +1,31 @@
 /*
- * Em desenvolvimento
+ * jQuery Plugin - Contador
+ * 
+ * @version     2.0
+ * @description 
+ * 
+ * Estrutura HTML de Exemplo:
+ *
+ * O elemento principal precisa ter dimensão definida
+ *
+ * <style type="text/css">
+ * .barra { width: 500px; height: 20px; }  
+ * </style>
+ *
+ * <div class="barra"></div>
+ *
+ * Forma de uso:
+ * 
+ * $('.barra').fiveContador({
+ *    inicio: 0, //Contador inicial
+ *    fim: 60, //Tempo em segundos até encerrar o contador
+ *    cor: '#000088', //Cor da barra de progresso
+ *    delay: 1000, //Delay em milisegundos da atualização do contador
+ *    onStart: function() {}, //Função callback a ser disparada antes do contador ser iniciado
+ *    onUpdate: function([value]) {}, //Função callback a ser disparada a cada progresso do contador. O argumento opcional value retorna o percentual do progrsso
+ *    onComplete: function() {}, //Função callback a ser disparada após o término do contador
+ * });
+ * 
  */
 (function($) {
     $.fn.extend({
