0dd9b22d5bc22d87db264cf607e766bcb30c7a94
asakurastar <asakurastar@gmail.com>
2011-05-05 18:42:04 -0300

    Novo plugin

---
+++ jquery.spread.js
@@ -0,0 +1,81 @@
+/*
+ * jQuery Plugin - Spread
+ * 
+ * @version     1.0
+ * 
+ * Estrutura HTML de Exemplo:
+ *
+ * <div class="spread">
+ *   <ul>
+ *     <li>Conteúdo</li>
+ *     <li>Outro conteúdo</li>
+ *     <li>Mais conteúdo</li>
+ *   </ul>
+ * </div>
+ *
+ * Forma de uso:
+ * 
+ * $('.spread').spread({
+ *   start: 0, //Define o item inicial aberto
+ *   opened: 200, //Define a largura (em pixels) do item aberto
+ *   closed: 50, //Define a largura (em pixels) dos itens fechado
+ * });
+ * 
+ */
+(function($) {
+    $.fn.extend({
+        spread: function(settings) {
+            var config = {
+                start: 0,
+                opened: 200,
+                closed: 50
+            };
+
+            if (settings) {
+                $.extend(config, settings);
+            }
+
+            return this.each(function() {
+                var $me = $(this);
+                var $parent = $me.find('ul');
+                var $items = $parent.find('li');
+
+                var methods = {
+                    init: function() {
+                        $items.css({
+                            'width': config.closed,
+                            'overflow': 'hidden',
+                            'float': 'left'
+                        });
+
+                        $items.eq(config.start).css('width', config.opened);
+
+                        methods.start();
+                    },
+
+                    start: function() {
+                        $items.hoverIntent(function() {
+                            $(this).animate({
+                                width: config.opened
+                            }, {
+                                duration: 500,
+                                queue: false
+                            });
+
+                            $items.animate({
+                                width: config.closed
+                            }, {
+                                duration: 500,
+                                queue: false
+                            });
+                        }, function() {});
+
+                    }
+
+                };
+
+                methods.init();
+            });
+        }
+    });
+})(jQuery);
