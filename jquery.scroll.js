3d7a0611ee323d11a83c3e80d38ff85793c6e771
unknown <dvetromilla@.intranet.fivecom.com.br>
2011-07-13 18:44:46 -0300

    Nova versão

--- jquery.scroll.js
+++ jquery.scroll.js
@@ -1,7 +1,7 @@
 /*!
  * jQuery Plugin - Scroll
  * 
- * @version     1.2
+ * @version     1.3
  * @description Plugin utilizado para criar barras de rolagem horizontal e/ou vertical. Inclui o plugin de Brandon Aaron para controle de mousewheel
  * @example http://jsfiddle.net/cuceta/mbLA9/
  * 
@@ -19,10 +19,11 @@
  * <div class="scrollLeft"></div>
  *
  * Forma de uso:
- * 
- * $('.scroll').scroll({
- *   top : '.scrollTop', //Elemento que representa a barra de rolagem vertical
- *   left : '.scrollLeft //Elemento que representa a barra de rolagem horizontal
+ * $(window).load(function() { //PRECISA ser dentro de $(window).load(callback);
+ *   $('.scroll').scroll({
+ *     top : '.scrollTop', //Elemento que representa a barra de rolagem vertical
+ *     left : '.scrollLeft //Elemento que representa a barra de rolagem horizontal
+ *   });
  * });
  *
  * Regras:
@@ -33,6 +34,7 @@
  * 1.0 Plugin liberado
  * 1.1 As setas do teclado permitem deslocar as barras de rolagem
  * 1.2 Correções para ajustes de controle de rolagem
+ * 1.3 Correções para cálculo de scrollHeight
  */
 
 /*! Copyright (c) 2010 Brandon Aaron (http://brandonaaron.net)
@@ -45,215 +47,178 @@
  * 
  * Requires: 1.2.2+
  */
-(function(d){function b(a){var c=[].slice.call(arguments,1),b=0,a=d.event.fix(a||window.event);a.type="mousewheel";a.wheelDelta&&(b=a.wheelDelta/120);a.detail&&(b=-a.detail/3);c.unshift(a,b);return d.event.handle.apply(this,c)}var c=["DOMMouseScroll","mousewheel"];d.event.special.mousewheel={setup:function(){if(this.addEventListener)for(var a=c.length;a;)this.addEventListener(c[--a],b,!1);else this.onmousewheel=b},teardown:function(){if(this.removeEventListener)for(var a=c.length;a;)this.removeEventListener(c[--a], b,!1);else this.onmousewheel=null}};d.fn.extend({mousewheel:function(a){return a?this.bind("mousewheel",a):this.trigger("mousewheel")},unmousewheel:function(a){return this.unbind("mousewheel",a)}})})(jQuery); 
+  $(window).load(function(){
+	  (function(d) {
+		function b(a) {
+			var c = [].slice.call(arguments, 1),
+				b = 0,
+				a = d.event.fix(a || window.event);
+			a.type = "mousewheel";
+			a.wheelDelta && (b = a.wheelDelta / 120);
+			a.detail && (b = -a.detail / 3);
+			c.unshift(a, b);
+			return d.event.handle.apply(this, c)
+		}
+		var c = ["DOMMouseScroll", "mousewheel"];
+		d.event.special.mousewheel = {
+			setup: function() {
+				if (this.addEventListener) for (var a = c.length; a;) this.addEventListener(c[--a], b, !1);
+				else this.onmousewheel = b
+			},
+			teardown: function() {
+				if (this.removeEventListener) for (var a = c.length; a;) this.removeEventListener(c[--a], b, !1);
+				else this.onmousewheel = null
+			}
+		};
+		d.fn.extend({
+			mousewheel: function(a) {
+				return a ? this.bind("mousewheel", a) : this.trigger("mousewheel")
+			},
+			unmousewheel: function(a) {
+				return this.unbind("mousewheel", a)
+			}
+		})
+	})(jQuery);
 
-(function($) {
-    $.fn.extend({
-        scroll: function(settings) {
-            var config = {
-                top: '.scrollTop',
-                left: '.scrollLeft'
-            };
+	(function($) {
+		$.fn.extend({
+			scroll: function(settings) {
+				var config = {
+					top: '.scrollTop',
+					left: '.scrollLeft'
+				};
 
-            if (settings) {
-                $.extend(config, settings);
-            }
+				if (settings) {
+					$.extend(config, settings);
+				}
 
-            return this.each(function() {
-                var me = this;
-                var $me = $(me);
-                var step = {
-                    X: 1,
-                    Y: 1
-                };
-                var size = {};
+				return this.each(function() {
+					var me = this;
+					var $me = $(me);
+					var step = {
+						X: 1,
+						Y: 1
+					};
 
-                var methods = {
-                    start: function() {
-                        methods.normalize();
-                        methods.bind();
-                    },
-                    normalize: function() {
-                        size.Y = $me.height() - (me.scrollHeight - $me.height());
-                        size.X = $me.width() - (me.scrollWidth - $me.width());
+					var methods = {
+						start: function() {
+							if ($me.height() < me.scrollHeight && $me.width() < me.scrollWidth) {
+								methods.normalize();
+								methods.bind();
+							}
+						},
+						normalize: function() {
+							var size = {};
 
-                        if (size.Y < 0) {
-                            size.Y = 18;
-                            step.Y = (me.scrollHeight - $me.height()) / ($me.height() - size.Y)
-                        }
+							size.Y = $me.height() - (me.scrollHeight - $me.height());
+							size.X = $me.width() - (me.scrollWidth - $me.width());
 
-                        if (size.X < 0) {
-                            size.X = 18;
-                            step.X = (me.scrollWidth - $me.width()) / ($me.width() - size.X)
-                        }
+							if (size.Y < 0) {
+								size.Y = 18;
+								step.Y = (me.scrollHeight - $me.height()) / ($me.height() - size.Y)
+							}
 
-                        if (methods.needVertical()) {
-                            $(config.top).css('height', size.Y);
-                        }
-                        else {
-                            $(config.top).css('display', 'none');
-                        }
-                        if (methods.needHorizontal()) {
-                            $(config.left).css('width', size.X);
-                        }
-                        else {
-                            $(config.left).css('display', 'none');
-                        }
+							if (size.X < 0) { 
+								size.X = 18;
+								step.X = (me.scrollWidth - $me.width()) / ($me.width() - size.X)
+							}
 
-                        if (methods.needVertical() || methods.needHorizontal()) {
-                            if (typeof $me.attr('tabindex') === 'undefined' || $me.attr('tabindex') === false) {
-                                $me.attr('tabindex', 9999);
-                            }
-                        }
-                    },
+							$(config.top).css('height', size.Y);
+							$(config.left).css('width', size.X);
 
-                    bind: function() {
-                        if (methods.needVertical()) {
-                            $(config.top).bind('mousedown.scroll', function(e) {
-                                $(this).data('scroll', {
-                                    start: 0,
-                                    Y: e.pageY - $(this).position().top
-                                });
+						},
 
-                                return false;
-                            });
-                        }
+						bind: function() {
+							$(config.top).bind('mousedown.scroll', function(e) {
+								$(this).data('scroll', {
+									start: 0,
+									Y: e.pageY - $(this).position().top
+								});
 
-                        if (methods.needHorizontal()) {
-                            $(config.left).bind('mousedown.scroll', function(e) {
-                                $(this).data('scroll', {
-                                    start: 0,
-                                    X: e.pageX - $(this).position().left
-                                });
+								curr = 1;
 
-                                return false;
-                            });
-                        }
+								return false;
+							});
 
-                        $(document).bind('mousemove.scroll', function(e) {
-                            if (methods.needVertical()) {
-                                if ($(config.top).data('scroll')) {
-                                    var position = e.pageY - $(config.top).data('scroll').Y;
+							$(config.left).bind('mousedown.scroll', function(e) {
+								$(this).data('scroll', {
+									start: 0,
+									X: e.pageX - $(this).position().left
+								});
 
-                                    if (position <= $(config.top).data('scroll').start) {
-                                        position = $(config.top).data('scroll').start;
-                                    }
-                                    if (position >= (($me.height() - $(config.top).height()) + $(config.top).data('scroll').start)) {
-                                        position = ($me.height() - $(config.top).height()) + $(config.top).data('scroll').start;
-                                    }
+								curr = 0;
 
-                                    $(config.top).css({
-                                        position: 'absolute',
-                                        left: $(config.top).position().left,
-                                        top: position
-                                    });
+								return false;
+							});
 
-                                    $me.scrollTop(position * step.Y);
-                                }
-                            }
+							$(document).bind('mousemove.scroll', function(e) {
+								if ($(config.top).data('scroll')) {
+									var position = e.pageY - $(config.top).data('scroll').Y;
 
-                            if (methods.needHorizontal()) {
-                                if ($(config.left).data('scroll')) {
-                                    var position = e.pageX - $(config.left).data('scroll').X;
+									if (position <= $(config.top).data('scroll').start) {
+										position = $(config.top).data('scroll').start;
+									}
+									if (position >= (($me.height() - $(config.top).height()) + $(config.top).data('scroll').start)) {
+										position = ($me.height() - $(config.top).height()) + $(config.top).data('scroll').start;
+									}
 
-                                    if (position <= $(config.left).data('scroll').start) {
-                                        position = $(config.left).data('scroll').start;
-                                    }
-                                    if (position >= (($me.width() - $(config.left).width()) + $(config.left).data('scroll').start)) {
-                                        position = ($me.width() - $(config.left).width()) + $(config.left).data('scroll').start;
-                                    }
+									$(config.top).css({
+										position: 'absolute',
+										left: $(config.top).position().left,
+										top: position
+									});
 
-                                    $(config.left).css({
-                                        position: 'absolute',
-                                        top: $(config.left).position().top,
-                                        left: position
-                                    });
+									$me.scrollTop(position * step.Y);
+								}
 
-                                    $me.scrollLeft(position * step.X);
-                                }
-                            }
-                            return false;
-                        });
+								if ($(config.left).data('scroll')) {
+									var position = e.pageX - $(config.left).data('scroll').X;
 
-                        if (methods.needVertical()) {
-                            $me.bind('mousewheel.scroll', function(event, delta) {
-                                $(this).scrollTop($(this).scrollTop() - delta * 10);
+									if (position <= $(config.left).data('scroll').start) {
+										position = $(config.left).data('scroll').start;
+									}
+									if (position >= (($me.width() - $(config.left).width()) + $(config.left).data('scroll').start)) {
+										position = ($me.width() - $(config.left).width()) + $(config.left).data('scroll').start;
+									}
 
-                                $(config.top).css({
-                                    position: 'absolute',
-                                    top: $(this).scrollTop() / step.Y
-                                });
+									$(config.left).css({
+										position: 'absolute',
+										top: $(config.left).position().top,
+										left: position
+									});
 
-                                return false;
-                            });
-                        }
+									$me.scrollLeft(position * step.X);
+								}
+								return false;
+							});
 
-                        $me.bind('keydown.scroll', function(e) {
-                            $(config.top).css('position', 'absolute');
-                            $(config.left).css('position', 'absolute');
+							$me.bind('mousewheel.scroll', function(event, delta) {
+								$(this).scrollTop($(this).scrollTop() - delta * 10);
 
-                            switch (e.which) {
-                            case 37:
-                                {
-                                    if (methods.needHorizontal()) {
-                                        $(this).scrollLeft($(this).scrollLeft() - 10);
-                                        $(config.left).css('left', $(this).scrollLeft() / step.X);
-                                    }
-                                    break;
-                                }
-                            case 38:
-                                {
-                                    if (methods.needVertical()) {
-                                        $(this).scrollTop($(this).scrollTop() - 10);
-                                        $(config.top).css('top', $(this).scrollTop() / step.Y);
-                                    }
-                                    break;
-                                }
-                            case 39:
-                                {
-                                    if (methods.needHorizontal()) {
-                                        $(this).scrollLeft($(this).scrollLeft() + 10);
-                                        $(config.left).css('left', $(this).scrollLeft() / step.X);
-                                    }
-                                    break;
-                                }
-                            case 40:
-                                {
-                                    if (methods.needVertical()) {
-                                        $(this).scrollTop($(this).scrollTop() + 10);
-                                        $(config.top).css('top', $(this).scrollTop() / step.Y);
-                                    }
-                                    break;
-                                }
-                            }
-                            return false;
-                        });
+								$(config.top).css({
+									position: 'absolute',
+									top: $(this).scrollTop() / step.Y
+								});
 
-                        $(document).bind('mouseup.scroll', function() {
-                            if (methods.needVertical()) {
-                                $(config.top).removeData('scroll');
-                            }
-                            if (methods.needHorizontal()) {
-                                $(config.left).removeData('scroll');
-                            }
-                        });
-                    },
+								return false;
+							});
 
-                    needVertical: function() {
-                        return ($me.height() < me.scrollHeight);
-                    },
+							$(document).bind('mouseup.scroll', function() {
+								$(config.top).removeData('scroll');
+								$(config.left).removeData('scroll');
 
-                    needHorizontal: function() {
-                        return ($me.width() < me.scrollWidth);
-                    },
+								curr = undefined;
+							});
+						},
 
-                    init: function() {
-                        methods.start();
-                    }
-                };
+						init: function() {
+							methods.start();
+						}
+					};
 
-                methods.init();
-            });
-        }
-    });
-})(jQuery);
+					methods.init();
+				});
+			}
+		});
+	})(jQuery);
+});
